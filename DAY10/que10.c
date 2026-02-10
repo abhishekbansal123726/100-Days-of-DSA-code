f7f9fa1ab1cac479c571ef4599ae80ab1443cef6
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2026-02-10 11:33:19 +0530

    work

---
+++ DAY10/que10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+
+    // Input string
+    scanf("%s", s);
+
+    int left = 0;
+    int right = strlen(s) - 1;
+
+    int flag = 1; // assume palindrome
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            flag = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (flag)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
