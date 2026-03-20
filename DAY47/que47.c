2527b90df4d103dbd98dd06862d71751a7fd171e
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2026-03-20 23:54:25 +0530

    work

---
+++ DAY47/que47.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int main() {
+    struct Node* root = newNode(3);
+    root->left = newNode(9);
+    root->right = newNode(20);
+    root->right->left = newNode(15);
+    root->right->right = newNode(7);
+
+    printf("Height of tree: %d", height(root));
+    return 0;
+}
