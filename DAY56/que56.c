acff099775ddcdbb679c04ce032fe078af65296f
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2026-03-30 10:13:59 +0530

    work

---
+++ DAY56/que56.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* buildTree(int arr[], int n, int i) {
+    if (i >= n || arr[i] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[i]);
+    root->left = buildTree(arr, n, 2*i + 1);
+    root->right = buildTree(arr, n, 2*i + 2);
+    return root;
+}
+
+int isMirror(struct Node* t1, struct Node* t2) {
+    if (t1 == NULL && t2 == NULL)
+        return 1;
+    if (t1 == NULL || t2 == NULL)
+        return 0;
+    return (t1->data == t2->data) &&
+           isMirror(t1->left, t2->right) &&
+           isMirror(t1->right, t2->left);
+}
+
+int isSymmetric(struct Node* root) {
+    if (root == NULL)
+        return 1;
+    return isMirror(root->left, root->right);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n, 0);
+
+    if (isSymmetric(root))
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
