0bd4a258c14c78d2037c7915b4fcddfbd46102e1
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2026-04-06 10:38:15 +0530

    work

---
+++ DAY65/que65.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int visited[100];
+
+int dfs(int v, int parent, int n, int adj[100][100]) {
+    visited[v] = 1;
+
+    for(int i = 0; i < n; i++) {
+        if(adj[v][i] == 1) {
+            if(visited[i] == 0) {
+                if(dfs(i, v, n, adj))
+                    return 1;
+            } else if(i != parent) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n;
+    int adj[100][100];
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(visited[i] == 0) {
+            if(dfs(i, -1, n, adj)) {
+                printf("YES");
+                return 0;
+            }
+        }
+    }
+
+    printf("NO");
+    return 0;
+}
